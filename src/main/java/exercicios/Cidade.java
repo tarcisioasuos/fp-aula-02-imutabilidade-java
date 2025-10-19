7c52de1817802dcdc5003f8eaa1560257ca7ca3f
tarcisioasuos <tarcisio.sousa.oliveira@gmail.com>
2025-10-19 13:19:18 -0300

    Criada classe Cidade com atributo nome e métodos de acesso

---
+++ src/main/java/exercicios/Cidade.java
@@ -0,0 +1,20 @@
+package exercicios;
+
+public class Cidade {
+    private String nome;
+
+    public Cidade(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String toString(){
+        return nome;
+    }
+
+}
+
