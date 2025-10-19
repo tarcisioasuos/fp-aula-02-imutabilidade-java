7e2ac35e85901cc16becebae0afc7fab8596bd98
tarcisioasuos <tarcisio.sousa.oliveira@gmail.com>
2025-10-19 13:46:57 -0300

    Criada classe Principal para testes manuais das classes e records

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,9 +1,30 @@
+import exercicios.Cidade;
+import exercicios.CidadeRecord;
+import exercicios.PessoaRecord;
+import exercicios.PessoaRecordShallow;
+
 /**
  * Classe para você testar suas implementações.
  * Veja o README para mais detalhes.
  */
 public class Principal {
     public static void main(final String[] args) {
+
+        Cidade cidadeMutavel = new Cidade("Tocantinópolis");
+        System.out.println("Cidade mutável antes: " + cidadeMutavel.getNome());
+        cidadeMutavel.setNome("Palmas");
+        System.out.println("Cidade mutável depois: " + cidadeMutavel.getNome());
+
+        CidadeRecord cidadeRecord = new CidadeRecord("Tocantinópolis");
+        System.out.println("CidadeRecord: " + cidadeRecord.nome());
+
+        PessoaRecordShallow pessoaShallow = new PessoaRecordShallow("Tarcísio", cidadeMutavel);
+        System.out.println("Pessoa shallow: " + pessoaShallow);
+        pessoaShallow.cidade().setNome("Palmas");
+        System.out.println("Pessoa shallow após a mudança da cidade: " + pessoaShallow);
+
+        PessoaRecord pessoaDeep = new PessoaRecord("Lanna", cidadeRecord);
+        System.out.println("Pessoa deep: " + pessoaDeep);
 
     }
 }
